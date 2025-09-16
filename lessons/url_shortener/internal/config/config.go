db0852992955db7bdd456ce6df2c685a274565b1
glkru <gl.krutoi@mail.ru>
2025-09-16 11:33:24 +0300

    FEAT(Url_api): собес, пока откладываю погружение
    ч2

--- lessons/url_shortener/internal/config/config.go
+++ lessons/url_shortener/internal/config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" envDefault:"development"`
+	StoragePath string `yaml:"storage_path" env:"STORAGE_PATH" envDefault:"/data" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+type HTTPServer struct {
+	Address      string        `yaml:"address" env:"HTTP_ADDR" envDefault:"localhost:8080"`
+	Timeout      time.Duration `yaml:"timeout" env:"HTTP_TIMEOUT" envDefault:"4s"`
+	idle_timeout time.Duration `yaml:"idle_timeout" env:"IDLE_TIMEOUT" envDefault:"60s"`
+}
+
+func MustLoad() {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		configPath = "config.yaml"
+
+	}
+}
