615db51630e79e8bfcc50608fa3b82283db737cb
glkru <gl.krutoi@mail.ru>
2025-09-22 14:40:16 +0300

    FEAT(internal/http-server/handlers/url/save) : дошел до валидации и о ужас еще одна новая библиотека.......

--- lessons/url_shortener/internal/http-server/handlers/url/save/save.go
+++ lessons/url_shortener/internal/http-server/handlers/url/save/save.go
@@ -4,6 +4,11 @@ import (
 	"log/slog"
 	"net/http"
 	resp "url_shortener/internal/lib/api/responce"
+	"url_shortener/internal/lib/logger/sl"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
 )
 
 type Request struct {
@@ -22,6 +27,32 @@ type URLSaver interface {
 
 func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "habdlers.url.save.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())))
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("%s: %s", op, sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			log.Error("%s: %s", op, sl.Err(err))
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
 
 	}
 }
