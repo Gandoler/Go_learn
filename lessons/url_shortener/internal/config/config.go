3836012b86558c80c60ed2e0a5fe5f40219afe1a
glkru <gl.krutoi@mail.ru>
2025-09-22 11:01:30 +0300

    FEAT (main) : добавил логер и загрузку конфига
    ps. адресс конфига передаеться через env

--- lessons/url_shortener/internal/config/config.go
+++ lessons/url_shortener/internal/config/config.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"log"
 	"os"
 	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
@@ -11,15 +14,26 @@ type Config struct {
 	HTTPServer  `yaml:"http_server"`
 }
 type HTTPServer struct {
-	Address      string        `yaml:"address" env:"HTTP_ADDR" envDefault:"localhost:8080"`
-	Timeout      time.Duration `yaml:"timeout" env:"HTTP_TIMEOUT" envDefault:"4s"`
-	idle_timeout time.Duration `yaml:"idle_timeout" env:"IDLE_TIMEOUT" envDefault:"60s"`
+	Address     string        `yaml:"address" env:"HTTP_ADDR" envDefault:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env:"HTTP_TIMEOUT" envDefault:"4s"`
+	idleTimeout time.Duration `yaml:"idle_timeout" env:"IDLE_TIMEOUT" envDefault:"60s"`
 }
 
-func MustLoad() {
+func MustLoad() *Config {
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
-		configPath = "config.yaml"
-
+		log.Fatal("CONFIG_PATH environment variable not set")
 	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("CONFIG_PATH %s does not exist", configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+
 }
