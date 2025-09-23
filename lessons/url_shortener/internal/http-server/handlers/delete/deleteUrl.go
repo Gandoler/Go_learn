986deed86a642ecf69d19685a54be299891dd84d
glkru <gl.krutoi@mail.ru>
2025-09-23 13:45:55 +0300

    FEAT (internal/http-server/handlers/delete/deleteUrl.go) : написал первый самостоятельный эндпоинт -ураааааа
    он просто удаляет по элиасу запись из бд

---
+++ lessons/url_shortener/internal/http-server/handlers/delete/deleteUrl.go
@@ -0,0 +1,52 @@
+package delete
+
+import (
+	"log/slog"
+	"net/http"
+	resp "url_shortener/internal/lib/api/responce"
+	"url_shortener/internal/lib/logger/sl"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=DeleteUrl
+
+type DeleteUrl interface {
+	DeleteUrl(alias string) error
+}
+
+func New(log *slog.Logger, urlDelete DeleteUrl) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.delete.New"
+
+		log := log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())))
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+
+			render.JSON(w, r, resp.Error("invalid request"))
+			return
+		}
+
+		err := urlDelete.DeleteUrl(alias)
+
+		if err != nil {
+			log.Error("failed to delete alias and url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("internal error"))
+
+			return
+		}
+
+		log.Info("delete alias and url", slog.String("by alias", alias))
+
+		// redirect to found url
+		w.WriteHeader(http.StatusNoContent)
+	}
+
+}
