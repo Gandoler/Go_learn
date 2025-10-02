eef2638e92a102dac1f36a92f320fa71117b4584
glkru <gl.krutoi@mail.ru>
2025-10-02 17:32:48 +0300

    UPDATE (lessons/grpc_url_shortener_service/sso/internal/grpc/auth/server.go) :  начал реализацюию сервера
    TODO остановился на ошибки с auth - решить ее

--- lessons/grpc_url_shortener_service/sso/internal/grpc/auth/server.go
+++ lessons/grpc_url_shortener_service/sso/internal/grpc/auth/server.go
@@ -5,18 +5,42 @@ import (
 
 	ssov1 "github.com/JustSkiv/protos/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+const emptyValue = 0
+
+type Auth interface {
+	Login(ctx context.Context, username string, password string, appId int) (token string, err error)
+	Register(ctx context.Context, username string, password string) (userID int, err error)
+	IsAdmin(ctx context.Context, userID int) (bool, error)
+}
 
 type serverApi struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
 func Register(grpcServer *grpc.Server) {
-	ssov1.RegisterAuthServer(grpcServer, &serverApi{})
+	ssov1.RegisterAuthServer(grpcServer, &serverApi{auth: auth})
 }
 
 func (s *serverApi) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
-	panic("implement me")
+	if req.GetEmail() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "email required")
+	}
+	if req.GetPassword() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "password required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return nil, status.Errorf(codes.InvalidArgument, "app id required")
+	}
+
+	return &ssov1.LoginResponse{
+		Token: "",
+	}, nil
 }
 
 func (s *serverApi) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
