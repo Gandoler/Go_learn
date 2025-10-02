1729945484fbe01aaffc86b7d1e8c5db6846b388
glkru <gl.krutoi@mail.ru>
2025-10-02 13:29:51 +0300

    FIX (lessons/grpc_url_shortener_service/sso/internal/config/config.go) : надо в атрибутах писать yaml  а не ym;

--- lessons/grpc_url_shortener_service/sso/internal/config/config.go
+++ lessons/grpc_url_shortener_service/sso/internal/config/config.go
@@ -9,15 +9,15 @@ import (
 )
 
 type Config struct {
-	Env         string        `yml:"env" env-default:"local"`
-	StoragePath string        `yml:"storage_path" env-required:"true"`
-	TokenTTl    time.Duration `yml:"token_ttl" env-required:"true"`
-	GRPC        GRPCConfig    `yml:"grpc"`
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	TokenTTl    time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
 }
 
 type GRPCConfig struct {
-	Port    int `yml:"port" `
-	Timeout int `yml:"timeout" `
+	Port    int           `yaml:"port" `
+	Timeout time.Duration `yaml:"timeout" `
 }
 
 func MustLoad() *Config {
@@ -26,12 +26,12 @@ func MustLoad() *Config {
 		panic("config file path is empty")
 	}
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		panic("config file not found" + path)
+		panic("config file not found\t" + path)
 	}
 	var config Config
 
 	if err := cleanenv.ReadConfig(path, &config); err != nil {
-		panic("failed to read config" + err.Error())
+		panic("failed to read config\t" + err.Error())
 	}
 	return &config
 
@@ -39,7 +39,7 @@ func MustLoad() *Config {
 
 func fetchConfigPath() string {
 	var path string
-	flag.StringVar(&path, "config", "config.yml", "config file path")
+	flag.StringVar(&path, "config", "", "path to config file")
 	flag.Parse()
 	if path == "" {
 		path = os.Getenv("CONFIG_PATH")
