6f66140765de5e8f52042b5d44929dc6c8806c82
glkru <gl.krutoi@mail.ru>
2025-10-02 13:52:56 +0300

    Update (lessons/grpc_url_shortener_service/sso/cmd/sso/main.go): добавил логгер

--- lessons/grpc_url_shortener_service/sso/cmd/sso/main.go
+++ lessons/grpc_url_shortener_service/sso/cmd/sso/main.go
@@ -1,12 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+	"sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Printf("%+v\n", cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting grpc server",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = setupPrettySlog()
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return logger
+
+}
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
