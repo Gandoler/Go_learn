bf4c26b227efc513607b4bb5e322e736841197b7
glkru <gl.krutoi@mail.ru>
2025-10-02 14:29:58 +0300

    FEAT (lessons/grpc_url_shortener_service/sso/internal/grpc/auth/server.go) : заглушка сервера

--- lessons/grpc_url_shortener_service/sso/internal/grpc/auth/server.go
+++ lessons/grpc_url_shortener_service/sso/internal/grpc/auth/server.go
@@ -1,7 +1,28 @@
 package auth
 
-import ssov1 "grpc_url_shortener_service/protos/gen/go/sso"
+import (
+	"context"
+
+	ssov1 "github.com/JustSkiv/protos/gen/go/sso"
+	"google.golang.org/grpc"
+)
 
 type serverApi struct {
 	ssov1.UnimplementedAuthServer
+}
+
+func Register(grpcServer *grpc.Server) {
+	ssov1.RegisterAuthServer(grpcServer, &serverApi{})
+}
+
+func (s *serverApi) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
+	panic("implement me")
+}
+
+func (s *serverApi) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
+	panic("implement me")
+}
+
+func (s *serverApi) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
+	panic("implement me")
 }
